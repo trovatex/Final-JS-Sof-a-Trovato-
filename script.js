3e9a03fd4cec904bd4fd30d433c12fa2368a4b73
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-07-05 09:53:49 -0300

    agregando prompt para total

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-let reserva, total
 
-let nombre = (prompt("Bienvenido al Hostel TROVATEX BUZIOS! Ingrese su nombre para continuar.")).toLowerCase();
+        let precioHabitacion = 5000
 
-// if (nombre != (isNaN)) {
-//   alert(`por favor, ingrese un nombre válido`)} // hacer que este ciclo continue, porque se corta
-    
-let dia = parseFloat (prompt(`Hola ${nombre}! Cuantos días te gustaría estar en el paraíso?`))
+        let nombre = (prompt("Bienvenido al Hostel TROVATEX BUZIOS! Ingrese su nombre para continuar.")).toLowerCase();
 
-let personas = parseFloat (prompt("Cuantas personas se van a hospedar en el hostel?"))
+        // if (nombre != (isNaN)) {
+        //   alert(`por favor, ingrese un nombre válido`)} // hacer que este ciclo continue, porque se corta
 
-const descuento = 0.15
-cupon = parseInt (prompt("Tiene cupon de descuento?"))
-if (cupon == "si") {
-  total = total * descuento
-} 
+        let dias = parseFloat(prompt(`Hola ${nombre}! Cuantos días te gustaría estar en el paraíso?`))
 
+        let personas = parseFloat(prompt("Cuantas personas se van a hospedar en el hostel?"))
 
+        const descuento = 0.15
 
+        cupon = parseInt(prompt("Tiene cupon de descuento?"))
 
-const multiplicar = (dias, personas) => dias * personas
-console.log(multiplicar)
+        if (cupon == "si") {
+            total = (( precioHabitacion - (precioHabitacion * descuento) ) * personas) * dias
+            console.log(total)
+        } else {
+            total = (precioHabitacion * personas) * dias
+            parseInt(prompt(total))
+        }
+
+// const multiplicar = (dias, personas) => dias * personas
+// console.log(multiplicar)
 
 // function precio (parametro1, parametro2) {
 //  return parametro1, parametro2
