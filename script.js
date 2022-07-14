8040d65d723e26000a57ca062492d5048220e50b
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-07-13 21:57:21 -0300

    agregando funciones al array

--- script.js
+++ script.js
@@ -50,12 +50,15 @@
 //  let multiplicar = multiplicar (dia * personas)
 //     console.log (precio)
 
+const multiplicar = (personas, dias) => personas * dias
 
 // function sumar (parametro1, parametro2) {
 //     return parametro1 + parametro2
 // }
 // let sumar = sumar (dias, personas)
 
+
+//////////////////////////////////////                   HASTA ACA ESTA TODO BIEN, AHORA
 
 //////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// ARRAYS ///////////////////////////////////////
@@ -70,6 +73,46 @@ class Hostel {
 
     }
 }
+//FUNCIONES
+function buscarHostel (hostels) {
+    let nombreHostel = prompt("Ingrese el nombre del Hostel que está buscando")
+
+    let hostelBuscado = hostels.find(hostels => hostels.nombre == nombreHostel)
+
+    if (hostelBuscado == undefined) {
+        alert ("Hostel no encontrado")
+    } else {
+        alert (`El hostel ${hostelBuscado} está disponible`)
+    }
+}
+
+function buscarPrecio (hostels) {
+    let precioHostel = parseInt(prompt("Ingrese un precio"))
+
+    let precioBuscado = hostels.filter(hostels => hostels.precio >= precio)
+
+    if (precioBuscado.length == 0) {
+        alert ("No hay hostels con dichas características")
+    } else {
+        alert (precioBuscado)
+    }
+}
+
+function ordenarHostels (hostels) {
+
+    let metodoOrdenamiento = parseInt(prompt("Ingrese 1 para ordenar de menor a mayor, 2 para ordenar de mayor a menor"))
+
+    if (metodoOrdenamiento === 1){
+
+        alert (hostels.sort((a,b) => a-b))
+    } else if (metodoOrdenamiento === 2){
+
+        alert (hostels.sort((a,b) => b-a))
+    }
+}
+
+
+
 
 const hostel1 = new Hostel ( "Trovatex Ferradurinha", "Brasil", 20, 7.5, [vistaAlMar, terraza, bañoCompartido])
 const hostel2 = new Hostel ( "Trovatex Potrero", "Costa Rica", 30, 8, [vistaAlMar, balcon, cocinaIndividual])
@@ -78,3 +121,31 @@ const hostel4 = new Hostel ( "Trovatex Barcelona", "España", 100, 9.5, [vistaAl
 
 const hostels = [hostel1, hostel2, hostel3, hostel4]
 console.log (hostels)   
+
+
+let respuesta
+//VALIDACIONES con un pequeño menú
+
+do {
+    respuesta = parseInt(prompt(`Ingrese número para:
+        1- Buscar un hostel 
+        2- Buscar hostel por precio
+        3- Ordenar de menor a mayor puntuación 
+    `))
+
+} while (respuesta >=1 && respuesta <=3)
+
+switch (respuesta) {
+        case 1:
+            buscarHostel(hostels)
+            break
+        case 2:
+            buscarPrecio(hostels)
+            break
+        case 3: 
+            ordenarHostels (hostels)
+            break
+        default:
+            alert ("Opción no válida")
+            break
+}
